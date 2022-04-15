35c42d47b624b32e4aa5f8d6eb7dbded5dc53f63
HamedZain <syrgst@gmail.com>
2022-04-16 02:58:27 +0300

    Use created components

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,23 @@
-import { FC } from 'react';
-import './App.scss';
+import { FC } from 'react'
+import './App.scss'
+import { Header } from '../components/Header/Header'
+import { Chat } from '../components/Chat/Chat'
+import { Stream } from '../components/Stream/Stream'
+import { CallControls } from '../components/CallControls/CallControls'
 
 export const App: FC = () => {
+
     return (
         <div className="App">
 
+            <Header />
+            <main className="App__content">
+                <div className="App__content__call">
+                    <Stream />
+                    <CallControls />
+                </div>
+                <Chat />
+            </main>
         </div>
-    );
+    )
 }
